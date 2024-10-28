d99d51937e1bdb88c2e251fcb27578e59274ac23
Lucas Oliveira <lucasvmo42@gmail.com>
2024-10-28 12:23:30 -0300

    primeira tarefa finalizada

--- Atividade1.java
+++ Atividade1.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Atividade1 {
     public static void main(String[] args) {
-        Scanner
+        Scanner leia = new Scanner(System.in);
+
+        float salario, abono, novoSalario;
+
+        System.out.println("Digite o salário");
+        salario = leia.nextFloat();
+        System.out.println("Digite o abono");
+        abono = leia.nextFloat();
+        novoSalario = salario+abono;
+        System.out.println("Seu novo salário é " + novoSalario);
+
     }
 }
